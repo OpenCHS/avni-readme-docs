652eb9ea9b03abaa754756aec640c96368bc8921
Vivek Singh <petmongrels@gmail.com>
2019-12-25 14:56:24 +0530

    create-database sample

---
+++ example-code-files/create-database.sh
@@ -0,0 +1,4 @@
+psql -U postgres -d postgres -c "create user openchs with password 'password' createrole"
+psql -U postgres -d postgres -c 'create database openchs with owner openchs'
+psql -U postgres -d openchs -c 'create extension if not exists "uuid-ossp"'
+psql -U postgres -d openchs -c 'create extension if not exists "ltree"'
